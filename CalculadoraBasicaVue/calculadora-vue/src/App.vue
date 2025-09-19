e181a4fc666f6c4ae8f6a426eec5d0350c4221df
Nicol-Beltran <beltrannicole384@gmail.com>
2025-09-18 22:12:34 -0500

    Calculadora basica inicial

---
+++ CalculadoraBasicaVue/calculadora-vue/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+  <h1>Mini herramientas</h1>
+  <hr/>
+
+  <MiCalculadora :value=5 />
+  <MiCalculadora :value=4 />
+
+  <MiCalculadoraTradicional :numero1=8 :numero2=6 />
+
+</div>
+</template>
+<script setup lang="ts">
+import MiCalculadora from './components/MiCalculadora.vue';
+import MiCalculadoraTradicional from './components/MiCalculadoraTradicional.vue';
+
+</script>
